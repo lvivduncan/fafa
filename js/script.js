8ab51dd85bef820344a06ebc1d6b496aa4f665e2
duncan <lvivduncan@gmail.com>
2020-07-27 18:54:02 +0300

    goods

--- js/script.js
+++ js/script.js
@@ -167,3 +167,17 @@ class LevusOnslide {
 }
 
 document.querySelector('#levus-slider') && new LevusOnslide('#levus-slider').init();
+
+// levus-anamate
+{
+    // висота видимої частини екрану
+    const height = document.documentElement.clientHeight;
+
+    // усі елементи, які мають бути анімовані
+    const elements = document.querySelectorAll('.levus-animate');
+
+    // робимо батьківський елемент оверфлоу:хідден, щоб сторінку не рвало
+    elements.forEach(element => element.parentElement.classList.add('hidden'));
+
+    window.addEventListener('scroll', () => elements.forEach(item => item.getBoundingClientRect().top < height && item.classList.add('show')));
+}
